aafeb6361ad04f9a900035a18e2613024c451e96
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-16 15:01:52 -0300

    agregue margin y padding a card de Cita

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -9,24 +9,24 @@ const Cita = () => {
             <Card.Title className="m-2 text-start">
               <h4>Mascota:</h4>
               <h5>Dueño:</h5>
-            </Card.Title> 
+            </Card.Title>
             <Card.Body className="fondoFormulario text-start">
               <div className="">
-                <div className="d-flex align-items-center mb-2">
+                <div className="d-md-flex align-items-center mb-2 text-center text-md-start">
                   <strong className="me-2">Fecha:</strong>
-                  <div className="bg-secondary-subtle px-2 py-1 w-100 rounded">
+                  <div className="bg-secondary-subtle py-1 w-100 rounded text-center">
                     10/10/2025
                   </div>
                 </div>
-                <div className="d-flex align-items-center mb-2">
+                <div className="d-md-flex  align-items-center mb-2 text-center text-md-start">
                   <strong className="me-2">Hora:</strong>
-                  <div className="bg-secondary-subtle px-2 py-1 w-100 rounded">
+                  <div className="bg-secondary-subtle py-1 w-100 rounded text-center">
                     10:50
                   </div>
                 </div>
-                <div className="d-flex align-items-center mb-2">
+                <div className="d-md-flex align-items-center mb-2 text-center text-md-start">
                   <strong className="me-2">Sintomas:</strong>
-                  <div className="bg-secondary-subtle px-2 py-1 w-100 rounded">
+                  <div className="bg-secondary-subtle py-1 w-100 rounded text-center">
                     Fiebre
                   </div>
                 </div>
