fc318f012016fa810fbea0c185015d338af7972a
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-21 15:53:14 -0300

    agregue prop datos en Cita

--- src/components/ListaCitas.jsx
+++ src/components/ListaCitas.jsx
@@ -5,9 +5,15 @@ const ListaCitas = ({datosProps}) => {
     <div>
       <div className="text-center bg-white rounded-3">
         <h2>Lista de citas</h2>
-        <div className="border border-1 border-dark-subtle rounded-3">
-          <Cita />
-        </div>
+        <div className="row row-cols-2 row-cols-md-3">
+      {datosProps.map((item, indice) => (
+        <Cita
+          key={indice}
+          datosProps={item}
+          
+        />
+      ))}
+    </div>
       </div>
     </div>
   );
